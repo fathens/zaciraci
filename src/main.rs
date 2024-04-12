4180bde2b979c5ff374d7238c20a0b14fe4b4bd5
沢谷 邦夫 <sawatani@doublejump.tokyo>
2024-04-12 09:23:55 +0900

    Replace warp with axum and implement counter functionality
    This commit replaces the Warp framework with Axum and implements a simple counter endpoint which counts the number of hits. A '/healthcheck' endpoint has also been added for health check purposes. The refactor improves handling of shared states between endpoints with improvements in concurrency management.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 #![deny(warnings)]
-use warp::Filter;
+
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU32, Ordering};
+use axum::{Router, routing::get};
+use axum::extract::State;
+
+struct AppState {
+    counter: AtomicU32,
+}
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+    let state = Arc::new( AppState {
+        counter: AtomicU32::new(0),
+    });
 
-    warp::serve(routes)
-        // ipv6 + ipv6 any addr
-        .run(([0, 0, 0, 0, 0, 0, 0, 0], 8080))
-        .await;
+    let app = Router::new()
+        .route("/healthcheck", get(|| async { "OK"}))
+        .route("/counter", get( get_counter)).with_state(Arc::clone(&state))
+        .route("/counter/increase", get( inc_counter)).with_state(Arc::clone(&state));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_counter(State(state): State<Arc<AppState>>) -> String {
+    let cur = state.counter.fetch_or(0, Ordering::Relaxed);
+    format!("Counter: {}", cur)
+}
+
+async fn inc_counter(State(state): State<Arc<AppState>>) -> String {
+    let cur = state.counter.fetch_add(1, Ordering::Relaxed);
+    format!("Counter: {}", cur)
 }
