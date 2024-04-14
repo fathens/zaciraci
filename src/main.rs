78a43d06cf936ab98396af8f1edeaa6637951c47
kunio <kunio@sawatani.jp>
2024-04-14 13:45:05 +0900

    Add 'once_cell' library and refactor AppState usage
    Introduced the 'once_cell' library to improve how AppState is created and used throughout the application. Instead of creating a new instance in the main function, an instance is now lazily initialized and shared globally, reducing redundancy and potential errors.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,25 @@ use axum::extract::State;
 use axum::{routing::get, Router};
 use persistence::Persistence;
 use std::sync::Arc;
+use once_cell::sync::Lazy;
 
 struct AppState {
     pstnce: Persistence,
 }
 
+static APP_STATE: Lazy<Arc<AppState>> = Lazy::new(|| {
+    let pstnce = Persistence::new().unwrap();
+    Arc::new(AppState { pstnce })
+});
+
 #[tokio::main]
 async fn main() {
-    let state = Arc::new(AppState {
-        pstnce: Persistence::new().unwrap(),
-    });
-
     let app = Router::new()
         .route("/healthcheck", get(|| async { "OK" }))
         .route("/counter", get(get_counter))
-        .with_state(Arc::clone(&state))
+        .with_state(Arc::clone(&APP_STATE))
         .route("/counter/increase", get(inc_counter))
-        .with_state(Arc::clone(&state));
+        .with_state(Arc::clone(&APP_STATE));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
