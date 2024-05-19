bae7e8867a7b3f7bb8d5fe6335e8b8fb9116d202
kunio <kunio@sawatani.jp>
2024-05-19 17:49:15 +0900

    Implement caching for group_by_out function in path.rs
    A new HashMap cached_by_out was added to the PoolsByToken struct to cache the outcome of the group_by_out function. The function itself was split into two: group_by_out which performs the operation, and get_groups_by_out, which now includes caching logic. The modifications aim to increase performance by preventing redundant computations.

--- src/ref_finance/path.rs
+++ src/ref_finance/path.rs
@@ -1,12 +1,14 @@
 use crate::ref_finance::pool_info::PoolInfoList;
 use near_sdk::AccountId;
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 mod edge;
 
+type EdgesByToken = HashMap<AccountId, edge::one_step::PathEdges>;
 pub struct PoolsByToken {
     by_in: HashMap<AccountId, Vec<Arc<edge::same_pool::CachedEdges>>>,
+    cached_by_out: Arc<Mutex<HashMap<AccountId, Arc<EdgesByToken>>>>,
 }
 
 #[allow(dead_code)]
@@ -21,44 +23,53 @@ impl PoolsByToken {
                     .push(edge::same_pool::CachedEdges::new(Arc::clone(pool)));
             });
         });
-        Self { by_in }
+        Self {
+            by_in,
+            cached_by_out: Arc::new(Mutex::new(HashMap::new())),
+        }
     }
 
     pub fn tokens(&self) -> Vec<AccountId> {
         self.by_in.keys().cloned().collect()
     }
 
-    pub fn group_by_out(
-        &self,
-        token_in: &AccountId,
-    ) -> HashMap<AccountId, edge::one_step::PathEdges> {
-        self.by_in
-            .get(token_in)
-            .map(|edges| {
-                let mut edges_by_token_out = HashMap::new();
-                edges.iter().for_each(|edge| {
-                    edge.pool
-                        .tokens()
-                        .filter(|&t| t != token_in)
-                        .for_each(|token_out| {
-                            edges_by_token_out
-                                .entry(token_out.clone())
-                                .or_insert_with(|| {
-                                    edge::one_step::PathEdges::new(
-                                        token_in.clone(),
-                                        token_out.clone(),
-                                    )
-                                })
-                                .push(Arc::clone(
-                                    &edge
-                                        .get_by_ids(token_in, token_out)
-                                        .expect("should be valid tokens"),
-                                ))
-                                .expect("should be same path")
-                        });
-                });
-                edges_by_token_out
+    pub fn get_groups_by_out(&self, token_in: &AccountId) -> Arc<EdgesByToken> {
+        self.cached_by_out
+            .lock()
+            .map(|mut cached_map| {
+                if let Some(cached) = cached_map.get(token_in) {
+                    return Arc::clone(cached);
+                }
+                let result = self.group_by_out(token_in).unwrap_or_default();
+                let cache = Arc::new(result);
+                cached_map.insert(token_in.clone(), Arc::clone(&cache));
+                cache
             })
             .unwrap_or_default()
+    }
+
+    fn group_by_out(&self, token_in: &AccountId) -> Option<EdgesByToken> {
+        self.by_in.get(token_in).map(|edges| {
+            let mut edges_by_token_out = HashMap::new();
+            edges.iter().for_each(|edge| {
+                edge.pool
+                    .tokens()
+                    .filter(|&t| t != token_in)
+                    .for_each(|token_out| {
+                        edges_by_token_out
+                            .entry(token_out.clone())
+                            .or_insert_with(|| {
+                                edge::one_step::PathEdges::new(token_in.clone(), token_out.clone())
+                            })
+                            .push(Arc::clone(
+                                &edge
+                                    .get_by_ids(token_in, token_out)
+                                    .expect("should be valid tokens"),
+                            ))
+                            .expect("should be same path")
+                    });
+            });
+            edges_by_token_out
+        })
     }
 }
