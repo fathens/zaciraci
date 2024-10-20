aea1182cd79158c93987a8dbd72c8208adb3db72
kunio <kunio@sawatani.jp>
2024-10-20 11:37:49 +0900

    Refactor testnet checking logic into a static variable
    Moved the testnet checking logic into a new static variable `IS_TESTNET` using `Lazy`. This reduces redundant calls and logs the selected network, improving code readability and maintainability.

--- src/ref_finance.rs
+++ src/ref_finance.rs
@@ -5,26 +5,37 @@ pub mod token_account;
 mod token_index;
 
 use crate::config;
+use crate::logging::*;
 use near_jsonrpc_client::JsonRpcClient;
 use near_sdk::AccountId;
 use once_cell::sync::Lazy;
 
+static IS_TESTNET: Lazy<bool> = Lazy::new(|| {
+    let str = config::get("USE_TESTNET").unwrap_or_default();
+    let log = DEFAULT.new(o!(
+        "function" => "IS_TESTNET",
+        "given_value" => format!("{}", str),
+    ));
+    let value = str.parse().unwrap_or_default();
+    if value {
+        info!(log, "Using testnet");
+    } else {
+        info!(log, "Using mainnet");
+    }
+    value
+});
+
 static CONTRACT_ADDRESS: Lazy<AccountId> = Lazy::new(|| {
-    if is_testnet() {
+    if *IS_TESTNET {
         "ref-finance-101.testnet".parse().unwrap()
     } else {
         "v2.ref-finance.near".parse().unwrap()
     }
 });
 static CLIENT: Lazy<JsonRpcClient> = Lazy::new(|| {
-    if is_testnet() {
+    if *IS_TESTNET {
         JsonRpcClient::connect(near_jsonrpc_client::NEAR_TESTNET_RPC_URL)
     } else {
         JsonRpcClient::connect(near_jsonrpc_client::NEAR_MAINNET_RPC_URL)
     }
 });
-
-pub fn is_testnet() -> bool {
-    let var = config::get("USE_TESTNET").unwrap_or_default();
-    var.parse().unwrap_or_default()
-}
