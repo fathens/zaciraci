26556fb0dfcb1100c39e4e789dd7cc8c4484dda8
kunio <kunio@sawatani.jp>
2024-10-23 22:46:57 +0900

    Add Account ID and signer support to Wallet struct
    Enhanced the Wallet struct by including an account_id field and new methods for retrieving and setting the account ID. Additionally, added the ability to create an InMemorySigner from the Wallet's keys, facilitating better key management and integration with other components.

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,6 +1,9 @@
 use crate::config;
 use crate::logging::*;
 use crate::Result;
+use near_crypto::SecretKey::ED25519;
+use near_crypto::{ED25519SecretKey, InMemorySigner};
+use near_sdk::AccountId;
 use once_cell::sync::Lazy;
 
 const DEFAULT_HDPATH: &str = "m/44'/397'/0'";
@@ -20,12 +23,18 @@ pub static WALLET: Lazy<Wallet> = Lazy::new(|| {
 
 #[derive(Clone)]
 pub struct Wallet {
+    account_id: AccountId,
     mnemonic: bip39::Mnemonic,
     hdpath: slipped10::BIP32Path,
     signing_key: ed25519_dalek::SigningKey,
 }
 
 impl Wallet {
+    fn get_account_id() -> Result<AccountId> {
+        let strval = config::get("ROOT_ACCOUNT_ID")?;
+        Ok(strval.parse()?)
+    }
+
     fn get_mnemonic() -> Result<bip39::Mnemonic> {
         let strval = config::get("ROOT_MNEMONIC")?;
         Ok(strval.parse()?)
@@ -36,12 +45,17 @@ impl Wallet {
         Ok(strval.parse()?)
     }
 
-    fn new(mnemonic: bip39::Mnemonic, hdpath: slipped10::BIP32Path) -> Result<Wallet> {
+    fn new(
+        account_id: AccountId,
+        mnemonic: bip39::Mnemonic,
+        hdpath: slipped10::BIP32Path,
+    ) -> Result<Wallet> {
         let log = DEFAULT.new(o!("function" => "Wallet::new"));
         debug!(log, "creating"; "hdpath" => %hdpath);
         let key = slipped10::derive_key_from_path(&mnemonic.to_seed(""), CURVE, &hdpath)?;
         let signing_key = ed25519_dalek::SigningKey::from_bytes(&key.key);
         let wallet = Wallet {
+            account_id,
             mnemonic,
             hdpath,
             signing_key,
@@ -51,9 +65,10 @@ impl Wallet {
     }
 
     pub fn new_from_config() -> Result<Wallet> {
+        let account_id = Self::get_account_id()?;
         let mnemonic = Self::get_mnemonic()?;
         let hdpath = Self::get_hdpath()?;
-        Self::new(mnemonic, hdpath)
+        Self::new(account_id, mnemonic, hdpath)
     }
 
     pub fn pub_base58(&self) -> String {
@@ -63,6 +78,11 @@ impl Wallet {
     pub fn derive(&self, index: i32) -> Result<Wallet> {
         let mut hdpath = self.hdpath.clone();
         hdpath.push(index as u32 + HARDEND);
-        Self::new(self.mnemonic.clone(), hdpath)
+        Self::new(self.account_id.clone(), self.mnemonic.clone(), hdpath)
+    }
+
+    pub fn signer(&self) -> InMemorySigner {
+        let seckey = ED25519SecretKey(self.signing_key.to_keypair_bytes());
+        InMemorySigner::from_secret_key(self.account_id.clone(), ED25519(seckey))
     }
 }
