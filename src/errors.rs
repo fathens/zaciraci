d188bc6e555c7b12aa947a8301f82cf71635460a
kunio <kunio@sawatani.jp>
2024-10-27 11:14:40 +0900

    Improve error handling for environment variables
    Updated `get` function in `config.rs` to provide detailed error context for environment variable errors. Enhanced the `Error` enum in `errors.rs` to include a specific variant for environment variable errors, replacing the previous plain string error handling.

--- src/errors.rs
+++ src/errors.rs
@@ -1,104 +1,84 @@
 use near_jsonrpc_client::errors::JsonRpcError;
 use near_jsonrpc_primitives::errors::RpcError;
 use near_primitives::account::id::ParseAccountError;
-use std::env::VarError;
 use std::fmt::{Debug, Display};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Error {
-    message: String,
+pub enum Error {
+    Plain(String),
+    EnvironmentVariable {
+        env_name: String,
+        err: std::env::VarError,
+    },
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
+        match self {
+            Error::Plain(msg) => write!(f, "{}", msg),
+            Error::EnvironmentVariable { env_name, err } => {
+                write!(f, "{}: {}", err, env_name)
+            }
+        }
     }
 }
 
 impl From<crate::ref_finance::errors::Error> for Error {
     fn from(e: crate::ref_finance::errors::Error) -> Error {
-        Error {
-            message: e.to_string(),
-        }
-    }
-}
-
-impl From<VarError> for Error {
-    fn from(e: VarError) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<deadpool::managed::PoolError<deadpool_diesel::Error>> for Error {
     fn from(e: deadpool::managed::PoolError<deadpool_diesel::Error>) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<ParseAccountError> for Error {
     fn from(e: ParseAccountError) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<diesel::result::Error> for Error {
     fn from(e: diesel::result::Error) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<deadpool_diesel::InteractError> for Error {
     fn from(e: deadpool_diesel::InteractError) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<RpcError> for Error {
     fn from(e: RpcError) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl<E: Display> From<JsonRpcError<E>> for Error {
     fn from(e: JsonRpcError<E>) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<serde_json::Error> for Error {
     fn from(e: serde_json::Error) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<bip39::Error> for Error {
     fn from(e: bip39::Error) -> Error {
-        Error {
-            message: e.to_string(),
-        }
+        Error::Plain(e.to_string())
     }
 }
 
 impl From<slipped10::Error> for Error {
     fn from(value: slipped10::Error) -> Self {
-        Error {
-            message: value.to_string(),
-        }
+        Error::Plain(value.to_string())
     }
 }
