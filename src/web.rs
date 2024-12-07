6b255e0141f0812a3fdc3545dfa51410ad386d14
kunio <kunio@sawatani.jp>
2024-12-07 16:02:20 +0900

    Add delete_all function to clear pool info table
    Introduce a new asynchronous function `delete_all` for removing all entries in the `pool_info` table. This function is now called before saving new pool information, ensuring the database is cleared first. This change helps maintain database integrity by preventing data duplication.

--- src/web.rs
+++ src/web.rs
@@ -77,6 +77,7 @@ async fn get_all_pools(State(_): State<Arc<AppState>>) -> String {
 
 async fn update_all_pools(State(_): State<Arc<AppState>>) -> String {
     let pools = pool_info::PoolInfoList::read_from_node().await.unwrap();
+    tables::pool_info::delete_all().await.unwrap();
     let n = pools.save_to_db().await.unwrap();
     format!("Pools: {n}")
 }
